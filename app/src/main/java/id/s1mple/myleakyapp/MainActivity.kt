29a342356685e079802d59c458e86bfdd7dfca67
harun7arrashid <harun4arrashid@gmail.com>
2021-11-25 17:05:04 +0700

    FInish

--- app/src/main/java/id/s1mple/myleakyapp/MainActivity.kt
+++ app/src/main/java/id/s1mple/myleakyapp/MainActivity.kt
@@ -1,11 +1,54 @@
 package id.s1mple.myleakyapp
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.s1mple.myleakyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var broadcastReceiver: BroadcastReceiver
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun registerBroadcastReceiver() {
+        broadcastReceiver = object : BroadcastReceiver() {
+
+            override fun onReceive(context: Context?, intent: Intent?) {
+                when (intent?.action) {
+                    Intent.ACTION_POWER_CONNECTED -> {
+                        binding.tvPowerStatus.text = getString(R.string.connect)
+                    }
+                    Intent.ACTION_POWER_DISCONNECTED -> {
+                        binding.tvPowerStatus.text = getString(R.string.disconect)
+                    }
+                }
+            }
+        }
+        val intentFilter = IntentFilter()
+        intentFilter.apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_POWER_DISCONNECTED)
+        }
+        registerReceiver(broadcastReceiver, intentFilter)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerBroadcastReceiver()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(broadcastReceiver)
     }
 }
